c8cfe96a8d215dff55da2617c8de5617ee08898c
IFELOLUWA AYOMIDE ROWAIYE <Rimoraproperties@gmail.com>
2025-12-30 07:59:13 +0100

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const prisma = require('../config/prisma');
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 router.post('/login', async (req, res) => {
